47320c62fbd0db716016df043bb1c7644d11c90b
CarwynKurt <carwynkurtarimado@gmail.com>
2022-11-13 10:14:07 +0800

    Display Menu

--- Contact Tracing Dictionary.py
+++ Contact Tracing Dictionary.py
@@ -1,6 +1,10 @@
 # Create Dictionary
 contact_dictionary = {}
 # Display Menu
+print("Menu")
+print("1 -> Add an item")
+print("2 -> Search")
+print("3 -> Exit")
 # Select an Item
 # Option 1 Result
 # Option 2 Result
