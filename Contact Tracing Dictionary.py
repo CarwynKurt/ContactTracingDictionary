67096e1888db93a6bdde738b40091c9151007fef
CarwynKurt <carwynkurtarimado@gmail.com>
2022-11-13 09:58:05 +0800

    Add program

--- Contact Tracing Dictionary.py
+++ Contact Tracing Dictionary.py
@@ -1,6 +1,10 @@
 # 1. Create Dictionary
 contact_dictionary = {}
 # 2. Display Menu
+print("Menu")
+print("1 -> Add an item")
+print("2 -> Search")
+print("3 -> Exit (y/n)")
 # 3. Select items in the menu
 # 4. Option 1 result
 # 5. Option 2 result
