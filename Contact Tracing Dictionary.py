74eca57dc4c9fcb6385518f5aac9d31dc89de0ec
CarwynKurt <carwynkurtarimado@gmail.com>
2022-11-13 10:33:33 +0800

    Option 1 Result

--- Contact Tracing Dictionary.py
+++ Contact Tracing Dictionary.py
@@ -8,6 +8,15 @@ print("3 -> Exit (y/n)")
 # Input user choice
 choice = int(input("What do you want to do? (1-3): "))
 # Option 1 Result
+if choice == 1:
+    name = input("Name: ")
+    gender = input("Gender: ")
+    age = input("Age: ")
+    address = input("Address: ")
+    number = input("Phone Number: ")
+    print("Saved!!")
+    contact_dictionary = {name : [gender, age, address, number]}
+    print(contact_dictionary)
 # Option 2 Result
 # Option 3 Result
 # Loop
