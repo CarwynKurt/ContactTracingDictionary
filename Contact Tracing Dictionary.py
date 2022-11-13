be2e13e0edea83c9d67e33b590a9d451aaec6acd
CarwynKurt <carwynkurtarimado@gmail.com>
2022-11-13 10:20:09 +0800

    Input User Choice

--- Contact Tracing Dictionary.py
+++ Contact Tracing Dictionary.py
@@ -6,6 +6,7 @@ print("1 -> Add an item")
 print("2 -> Search")
 print("3 -> Exit (y/n)")
 # Input user choice
+choice = int(input("What do you want to do? (1-3): "))
 # Option 1 Result
 # Option 2 Result
 # Option 3 Result
